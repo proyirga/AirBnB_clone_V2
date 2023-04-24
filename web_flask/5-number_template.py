a15ae0767b0badcf567688ad4fef70c32b76c1a2
Yirga Mulaw <infoyirga12@gmail.com>
2023-04-24 11:46:45 +0300

    Template correction

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -60,9 +60,8 @@ def isnumber(n):
         return '{} is a number'.format(n)
 
 
-@app.route('/number_template/', strict_slashes=False)
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def numbertemplate(n=None):
+def numbertemplate(n):
     if isinstance(n, int):
         return render_template('5-number.html', n=n)
 
